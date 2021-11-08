c022aa6c5f3b7d87890110cb8f670951e76eae0e
hkbagci <hkerembagci@gmail.com>
2021-11-08 19:25:10 +0300

    Still working...

--- src/main/java/tr/com/hkerembagci/routebetweencountries/rest/RestClient.java
+++ src/main/java/tr/com/hkerembagci/routebetweencountries/rest/RestClient.java
@@ -1,46 +1,54 @@
 package tr.com.hkerembagci.routebetweencountries.rest;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 import tr.com.hkerembagci.routebetweencountries.entity.Root;
 import tr.com.hkerembagci.routebetweencountries.exception.CountryNotFoundException;
-import tr.com.hkerembagci.routebetweencountries.model.Graph;
-import tr.com.hkerembagci.routebetweencountries.model.Node;
 
+import java.io.IOException;
 import java.util.*;
 
 @Component
 @Data
 public class RestClient {
 
-    private static final String URL_COUNTRIES_JSON = "https://raw.githubusercontent.com/mledoze/countries/master/countries.json";
+    //    If you can access the json file over Internet, you can use as below.
+    //    private static final String URL_COUNTRIES_JSON = "https://raw.githubusercontent.com/mledoze/countries/master/countries.json";
+    private static final String URL_COUNTRIES_JSON = "D:\\HKB\\route-between-countries-master\\countries.json";
+    @Value("classpath:countries.json")
+    private Resource resourceFile;
     private static List<Root> countryList;
     private static int minimumRoute = 0;
     private static Root sourceCountry;
     private static Root destinationCountry;
+    private static List<String> bestRouteList;
 
-    public void getCountryList() throws JsonProcessingException {
-        RestTemplate restTemplate = new RestTemplate();
+    public void getCountryList() throws IOException {
+        /*
+            If you can access the json file over Internet, you can use this.
+            RestTemplate restTemplate = new RestTemplate();
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonResult = restTemplate.getForObject(URL_COUNTRIES_JSON, String.class);
+            countryList = objectMapper.readValue(jsonResult, new TypeReference<List<Root>>() {});
+         */
+
         ObjectMapper objectMapper = new ObjectMapper();
-        String jsonResult = restTemplate.getForObject(URL_COUNTRIES_JSON, String.class);
-        countryList = objectMapper.readValue(jsonResult, new TypeReference<List<Root>>() {
-        });
+        countryList = Arrays.asList(objectMapper.readValue(resourceFile.getFile(), Root[].class));
     }
 
-    public String findRouteByCountries(String source, String destination) throws CountryNotFoundException, JsonProcessingException {
+    public String findRouteByCountries(String source, String destination) throws CountryNotFoundException, IOException {
         if (null == countryList) {
             getCountryList();
         }
-        sourceCountry = getCountryByCca3(source, countryList);
+        sourceCountry = getCountryByCca3(source);
         if (null == sourceCountry) {
             throw new CountryNotFoundException("Source country cannot found: " + source);
         }
-        destinationCountry = getCountryByCca3(destination, countryList);
+        destinationCountry = getCountryByCca3(destination);
         if (null == destinationCountry) {
             throw new CountryNotFoundException("Destination country cannot found: " + destination);
         }
@@ -61,43 +69,40 @@ public class RestClient {
     }
 
     private List<String> findBestRoute() {
-        List<String> bestRoute = new ArrayList<>();
-        for (String border : sourceCountry.getBorders()) {
-            List<String> tempRouteList = createRouteList(sourceCountry, destinationCountry.getCca3(), new ArrayList<>());
-            if (null != tempRouteList) {
-                minimumRoute = tempRouteList.size();
-                bestRoute = tempRouteList;
-            }
-        }
-        return bestRoute;
+        bestRouteList = new ArrayList<>();
+        Set<Map<String, List<String>>> routeSet = new HashSet<>();
+        createRouteList(sourceCountry, routeSet);
+        return bestRouteList;
     }
 
-    private List<String> createRouteList(Root source, String destination, List<String> routeMapList) {
-        if (null != source.getBorders()) {
-            for (String border : source.getBorders()) {
-                if (routeMapList.isEmpty()) {
-                    routeMapList.add(sourceCountry.getCca3());
-                } else {
-                    if (routeMapList.contains(border)) {
-                        continue;
-                    } else {
-                        routeMapList.add(border);
+    private void createRouteList(Root source, Set<Map<String, List<String>>> routeSet) {
+        bestRouteList.add(source.getCca3());
+        routeSet.add(getNodeList(source));
+        for (Map<String, List<String>> set : routeSet) {
+            for (Map.Entry<String, List<String>> entry : set.entrySet()) {
+                if (entry.getValue().contains(destinationCountry.getCca3())) {
+                    bestRouteList.add(destinationCountry.getCca3());
+                    return;
+                }
+                for (String borderCountry : entry.getValue()) {
+                    if (!bestRouteList.contains(borderCountry)) {
+                        bestRouteList.add(source.getCca3());
                     }
+                    Root tempSource = getCountryByCca3(borderCountry);
+                    createRouteList(tempSource, routeSet);
+                    return;
                 }
-                if (source.getBorders().contains(destinationCountry.getCca3())) {
-                    routeMapList.add(destinationCountry.getCca3());
-                    return routeMapList;
-                }
-                if (minimumRoute != 0 && minimumRoute < routeMapList.size()) {
-                    return null;
-                }
-                createRouteList(getCountryByCca3(border, countryList), destination, routeMapList);
             }
         }
-        return routeMapList;
     }
 
-    public Root getCountryByCca3(String country, List<Root> countryList) {
+    private Map<String, List<String>> getNodeList(Root source) {
+        Map<String, List<String>> tempMap = new HashMap<>();
+        tempMap.put(source.getCca3(), source.getBorders());
+        return tempMap;
+    }
+
+    public Root getCountryByCca3(String country) {
         for (Root root : countryList) {
             if (root.getCca3().equals(country)) {
                 return root;
--- src/main/java/tr/com/hkerembagci/routebetweencountries/rest/RestService.java
+++ src/main/java/tr/com/hkerembagci/routebetweencountries/rest/RestService.java
@@ -1,10 +1,10 @@
 package tr.com.hkerembagci.routebetweencountries.rest;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tr.com.hkerembagci.routebetweencountries.exception.CountryNotFoundException;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping(path = "/routing")
@@ -18,7 +18,7 @@ public class RestService {
 
     @GetMapping(value = "/{source}/{destination}", produces = "application/json")
     @ResponseBody
-    public ResponseEntity<String> findRouteBetweenCountries(@PathVariable String source, @PathVariable String destination) throws CountryNotFoundException, JsonProcessingException {
+    public ResponseEntity<String> findRouteBetweenCountries(@PathVariable String source, @PathVariable String destination) throws CountryNotFoundException, IOException {
         return ResponseEntity.ok(restClient.findRouteByCountries(source, destination));
     }
 
